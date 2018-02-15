8b7c9198c0f10574ca50973a3c91e5b44a57221b
pawelz <t03c10@gmail.com>
2018-02-15 21:07:18 +0100

    task 9.2 corrected

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-
 var allNames = femaleNames.concat(maleNames);
 
-
-
 function addName(newName) {
-if (allNames.indexOf(newName) === -1)
-	{allNames.push(newName);
-		console.log ('Dodano imię "' +newName + '".')}
-	else {console.log('Imię "' + newName + '" już występuje w bazie.')}
+	if (allNames.indexOf(newName) === -1) {
+		allNames.push(newName);
+		console.log ('Dodano imię "' +newName + '".');
+	}
+		else {
+			console.log('Imię "' + newName + '" już występuje w bazie.');
+		}
 }
 
-addName('Ola')
-addName('Marian')
+addName('Ola');
+addName('Marian');
 console.log(allNames);
